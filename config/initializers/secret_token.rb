2490a236865287a00fbb9087dfc27ec5bfbaa06c
Rudy Rigot <rudy.rigot@gmail.com>
2013-12-07 12:42:45 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Letshang::Application.config.secret_key_base = '679aaebda8444b4aa1c34ac38931285d8d6ab9631648636bc0363ea3bfffa0d40745fe733635bbb3670121a89fbb01dd3a21d0898f17ddfc5bfdff22e7ae34d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Letshang::Application.config.session_store :cookie_store, key: '_letshang_session'
