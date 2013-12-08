8740bbf12d909b462063f16c4aec82aba50a6b99
Rudy Rigot <rudy.rigot@gmail.com>
2013-12-08 01:19:15 -0800

    Fix for mobile: position fixed doesn't hide anything

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
 	if (
-	$('main#index').length &&
+	$('main#index').length /*&&
 	("standalone" in window.navigator) &&
-	!window.navigator.standalone
+	!window.navigator.standalone*/
 	){
 		setTimeout(function(){
-			if (navigator.userAgent.indexOf('iPhone') > 0) $('#appModeNoteiPhone').slideDown();
-			else if (navigator.userAgent.indexOf('iPad') > 0) $('#appModeNoteiOS').slideDown();
-			else $('#appModeNoteOther').slideDown();
+			if (navigator.userAgent.indexOf('iPhone') > 0) {
+				$('#appModeNoteiPhone').slideDown();
+				setTimeout(function(){ $('main').css('margin-bottom', $('#appModeNoteiPhone').height()+'px'); }, 500);
+			}
+			else if (navigator.userAgent.indexOf('iPad') > 0) {
+				$('#appModeNoteiOS').slideDown();
+				setTimeout(function(){ $('main').css('margin-bottom', $('#appModeNoteiOS').height()+'px'); }, 500);
+			}
+			else {
+				$('#appModeNoteOther').slideDown();
+				setTimeout(function(){ $('main').css('margin-bottom', $('#appModeNoteOther').height()+'px'); }, 500);
+			}
 		}, 1000);
-		$('.closeAppMode').click(function(){ $(this).parent().slideToggle(); });
+		$('.closeAppMode').click(function(){ $(this).parent().slideToggle(); $('main').css('margin-bottom', '0'); });
 	}
 });
